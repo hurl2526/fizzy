07ecf17be66f94f1ac2f15ba9c13094fc61c6c06
Patrick <patrick.hurley@codeimmersives.com>
2020-02-04 14:34:32 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 function fizzy(num) {
-
+  if (num % 3 === 0 && num % 5 === 0){
+    return "FizzBuzz"}
+else if (num % 3 === 0){
+  return "Fizz"
+}
+else if (num % 5 === 0){
+  return "Buzz"
+}
+else if (num % 3 !== 0 && num % 5 !== 0)
+return num;
 }
 
 module.exports = {
